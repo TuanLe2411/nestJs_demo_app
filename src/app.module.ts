9e1c43069a81f40852cacfce6ea9b97cfdbc8d65
TuanLe2411 <tuanldhaihaua@gmail.com>
2023-02-13 18:01:12 +0700

    add student schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsModule } from './students/students.module';
+import config from './configs/configurations';
+import { StudentsModule } from './modules/students.module';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./environments/.${
+        process.env.NODE_ENV || 'development'
+      }.env`,
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongoConfigs.url'),
+      }),
+      inject: [ConfigService],
+    }),
+    StudentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/configurations.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  mongoConfigs: {
+    url: process.env.MONGO_URL,
+  },
+});
